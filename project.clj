079242d040b6a36983756223b0e28af52edf2de8
snozaki <snozaki@netprotections.co.jp>
2022-04-17 22:12:23 +0900

    微修正

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject todo-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "TODO管理REST API"
+  :url "https://github.com/simonNozaki/todo-clj.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
